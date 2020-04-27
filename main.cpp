b21a863f3567dd411a3a45fd173e99ae3a90e0e9
Samuel <lunarhive@gmail.com>
2020-04-27 05:07:04 +0000

    unstable version 1.0 build 1.0

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <stdlib.h>
+#include<fstream>
+#include <ctime>
+#include <cstdio>
 
 using namespace std;
 
@@ -156,8 +160,88 @@ struct INVENTORY
 {
    map <string,float> parts; //a map of parts and prices.
 
+    /*Operations*/
+    //Find part
+   bool FindRequestedPart(string part){
+
+       if(parts.find(part)->first == part)
+        return true;
+   }
+    //Setters
+   float setRequestedPartPrice(string part, float price)
+   {
+       parts.find(part)->second = price;
+       cout <<"Successful"<<endl;
+   }
+
+   //Getters
+   float GetRequestedPartPrice(string part){
+    return parts.find(part)->second;
+   }
+
+   string GetRequestedPart(string part)
+   {
+       return parts.find(part)->first;
+   }
+
+
+
 };
-// TODO (FAMILY PC#1#): inventory list creation
+
+struct TRANSACTION
+{
+
+
+};
+
+struct INVOICE
+{
+    int hoursSpent; //hours spent on the bus
+    int quantity;
+
+    /*Operations*/
+    //Getters
+    int GetHoursSpent(){
+        return hoursSpent;
+    }
+
+    int GetQuantity(){
+    return quantity;
+    }
+
+
+    //Setters
+    int SetHoursSpent(int hours){
+        this->hoursSpent = hours;
+    }
+
+    int SetQuantity(int quantity){
+     this->quantity = quantity;
+    }
+
+
+
+   float CalculateInovice(struct INVENTORY inventory, int quantity,string part){
+
+       return (inventory.GetRequestedPartPrice(part)* quantity);
+   }
+
+};
+
+
+/* FIXME (FAMILY PC#1#04/27/20): Next Build
+1) Insert a measure of control for parts requested and bus fault detected.
+2) Make registration number accepts strings plus whitespaces
+3) Make billing address accept strings plus whitespaces
+4) Make name to accept strings plus whitespaces
+*/
+
+/* NOTE (FAMILY PC#1#04/27/20): Refactoring
+1)  Bus owner can have more than one bus hence bus assignment to bus owner has to be dynamic
+2) Bus can have more than one faults hence faults should be in a collection or container.
+*/
+
+
 //main function
 int main()
 {
@@ -167,9 +251,9 @@ int main()
     cout <<"*************************************"<<endl;
 
     INVENTORY adumList; //inventory of parts
-    adumList.parts.insert(pair<string, float>("headlights", 25.3));
-    adumList.parts.insert(pair<string, float>("bolts", 15.23));
-    adumList.parts.insert(pair<string, float>("spark plugs", 30.5));
+    adumList.parts.insert(pair<string, float>("headlights", 0));
+    adumList.parts.insert(pair<string, float>("bolts", 0));
+    adumList.parts.insert(pair<string, float>("spark plugs", 0));
 
 
     //enter bus details
@@ -184,9 +268,32 @@ int main()
     cin >> regNumber;
     cout <<"Is Bus New(Y/N)?:";
     cin >> isBusNew;
+    switch(isBusNew){ //check for input invalid input
+    case 'Y':
+        ;
+        break;
+    case 'N':
+        ;
+        break;
+    default:
+        cout <<"INVALID INPUT:"<<isBusNew<<endl;
+        return -1;
+    }
     cout <<"Is Bus Faulty(Y/N)?:";
     cin >> isBusFaulty;
+    switch(isBusFaulty){ //check for input invalid input
+    case 'Y':
+        ;
+        break;
+    case 'N':
+        ;
+        break;
+    default:
+        cout <<"INVALID INPUT:"<<isBusFaulty<<endl;
+        return -1;
+    }
 
+    cout <<endl;
 
     //enter bus owner details
     string name,billingAddress;
@@ -216,10 +323,15 @@ int main()
       */
 
     //Add new bus to the system, all buses are faulty
-    if(isBusNew == 'Y')
+    if(isBusNew == 'N' || isBusNew == 'Y')
    {
     BUS* bus1;
     bus1 = new BUS; //new bus in station
+
+    time_t rawtime; string timestamp;
+    time(&rawtime);
+    timestamp = ctime(&rawtime); //timestamp created.
+    cout <<"Timestamp:"<< timestamp << endl;
 
     BUSOWNER* busowner1;
     busowner1 = new BUSOWNER; //new busowner in station
@@ -242,15 +354,20 @@ int main()
         //assume that a
         busowner1->bus = *bus1;
 
-        busowner1->bus.DisplayBusDetails();
+        //busowner1->bus.DisplayBusDetails();
 
     }
     else
-        cout <<"Error"<<endl;
+        cout <<"Memory allocation error"<<endl;
 
-        if(isBusFaulty == 'Y')
+        //schedule a repair
+        if(isBusFaulty != 'N')
         {
-            cout <<"Repair Mode"<<endl;
+            system("pause");
+            //clear screen
+            system("cls");
+
+            cout <<"\t\t***Repair Mode***"<<endl;
             //mechanic and helper assigned to bus
             MECHANIC mech;
             mech.mechanic.name = "Kwame";
@@ -258,16 +375,88 @@ int main()
             HELPER help;
             help.helper.name = "John";
 
-            cout <<"\nMechanic,"<<mech.mechanic.name<<" and "<<"Helper,"
-            <<help.helper.name<<" assigned to car with registration number: "<<bus1->GetRegNumber() << endl;
+            //display mechanic, task details
+            cout <<"\nMechanic:"<<mech.mechanic.name<<endl<<"Helper:"
+            <<help.helper.name<<endl<<"Task:"<<"assigned to "<<"bus with registration number,"<<bus1->GetRegNumber()
+            << endl <<"Fault:"<<bus1->GetBusFault()<<endl;
 
+            //find parts
+            cout <<"Parts Request."<<endl;
+            cout <<"Search requeseted part.."<<endl;
+            cout <<"Enter part:"<<endl;
+            string parts;
+            cin >> parts;
+
+            if(adumList.FindRequestedPart(parts))
+
+            {
+              cout <<"Match Found!"<<endl;
+              cout <<"Price requested part:GHC"<<flush;
+              float price;
+              cin >> price;
+              adumList.setRequestedPartPrice(parts,price);
+
+              cout <<"Enter hours spent:";
+              int hours;
+              cin >> hours;
+
+              cout<<"Enter Quantity used:"<<flush;
+              int quantity;
+              cin >> quantity;
+
+              //produce invoice
+              INVOICE invoice1;
+              invoice1.SetQuantity(quantity);
+
+              system("pause");
+              system("cls");  // new screen
+              cout <<"\t\t***Generating Receipt***"<<endl;
+              //invoice1.SetHoursSpent(hours);
+              //cout <<invoice1.CalculateInovice(adumList,quantity,parts)<<endl;
+
+              ofstream out("receipt.txt"); //create receipt
+              if(!out)
+              {
+                  cout <<"Cannot open file. \n";
+                  return 1;
+              }
+               else{
+                 out<<"ADUM BUS REPAIR SHOP INVOICE\n" <<"Date of transaction:"<<timestamp<<endl
+               <<"Type of repair:"<<bus1->GetBusFault()<<endl<<"Parts used:"<<adumList.GetRequestedPart(parts)<<endl
+               <<"cost of parts:GHC"<<invoice1.CalculateInovice(adumList,quantity,parts) << endl
+               <<"Mechanic assigned:"<<mech.mechanic.name<<endl<<"Helper assigned:"<<help.helper.name<<endl<<"Thank you for visting Adum Bus Repair Shop,"
+               <<busowner1->GetName()<<endl;
+
+               out.close();
+               }
+
+               //check file is not in error
+              if(!out.goodbit)
+                {
+                    cout <<"Invoice generated successfully"<<endl;
+                    delete bus1;
+                    delete busowner1;
+                }
+
+                //
+
+
+
+
+
+            }
+            else
+            cout <<"Not available in inventory"<<endl;
 
         }
-   }
+        else{
+            cout <<"No Repair needed"<<"\nBus is not faulty"<<endl;
+            return -1;
+        }
+
+        }
    else
-   {
-       cout <<"----"<<endl;
-   }
+    ; //null statement
 
 
 
